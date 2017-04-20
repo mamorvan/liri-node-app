f4f1393690d2329b73ba64d84cd7f89dd53b3c50
Mary Morvan <mamorvan@yahoo.com>
2017-04-20 09:45:14 -0400

    add twitter key and random text song :sunflower:

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log("this is loaded");
 
 exports.twitterKeys = {
-	consumer_key: "",
-	consumer_secret: "",
-	access_token_key: "",
-	access_token_secret: ""
+	consumer_key: "w8IEmQ9R1bCWOClNMSBw21FFf",
+	consumer_secret: "tXw81mCrfSneZiKvMTfNxtQLLkaJuFtp7DxYRboWlpx6L69Ope",
+	access_token_key: "854807601661521920-kXB24QkQfn3ZT95INb10tHvBoc72c0b",
+	access_token_secret: "onz1rnibNTfCoB53etKiCEEIVieBUR4XwiyKVWb2BRmFm"
 }
